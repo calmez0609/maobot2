9d6893e3414c3ceadc4432ed0adff6386637c8a5
calmez0609 <33241538+calmez0609@users.noreply.github.com>
2020-01-12 21:18:39 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,100 +1,55 @@
+
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"image/color"
 	"log"
+	"net/http"
 	"os"
 
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/vg/draw"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
-	xys, err := readData("data.txt")
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
 	if err != nil {
-		log.Fatalf("could not read data.txt: %v", err)
+		log.Fatal(err)
 	}
-	_ = xys
 
-	err = plotData("out.png", xys)
-	if err != nil {
-		log.Fatalf("could not plot data: %v", err)
-	}
-}
-
-type xy struct{ x, y float64 }
-
-func readData(path string) (plotter.XYs, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	var xys plotter.XYs
-	s := bufio.NewScanner(f)
-	for s.Scan() {
-		var x, y float64
-		_, err := fmt.Sscanf(s.Text(), "%f,%f", &x, &y)
+	// Setup HTTP Server for receiving requests from LINE platform
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req)
 		if err != nil {
-			log.Printf("discarding bad data point %q: %v", s.Text(), err)
-			continue
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
+			return
 		}
-		xys = append(xys, struct{ X, Y float64 }{x, y})
-	}
-	if err := s.Err(); err != nil {
-		return nil, fmt.Errorf("could not scan: %v", err)
-	}
-	return xys, nil
-}
-
-func plotData(path string, xys plotter.XYs) error {
-	f, err := os.Create(path)
-	if err != nil {
-		return fmt.Errorf("could not create %s: %v", path, err)
-	}
-
-	p, err := plot.New()
-	if err != nil {
-		return fmt.Errorf("could not create plot: %v", err)
-	}
-
-	// create scatter with all data points
-	s, err := plotter.NewScatter(xys)
-	if err != nil {
-		return fmt.Errorf("could not create scatter: %v", err)
-	}
-	s.GlyphStyle.Shape = draw.CrossGlyph{}
-	s.Color = color.RGBA{R: 255, A: 255}
-	p.Add(s)
-
-	var x, c float64
-	x = 1.2
-	c = -3
-
-	// create fake linear regression result
-	l, err := plotter.NewLine(plotter.XYs{
-		{3, 3*x + c}, {20, 20*x + c},
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.StickerMessage:
+					replyMessage := fmt.Sprintf(
+						"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
 	})
-	if err != nil {
-		return fmt.Errorf("could not create line: %v", err)
+	// This is just sample code.
+	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
 	}
-	p.Add(l)
-
-	wt, err := p.WriterTo(256, 256, "png")
-	if err != nil {
-		return fmt.Errorf("could not create writer: %v", err)
-	}
-	_, err = wt.WriteTo(f)
-	if err != nil {
-		return fmt.Errorf("could not write to %s: %v", path, err)
-	}
-
-	if err := f.Close(); err != nil {
-		return fmt.Errorf("could not close %s: %v", path, err)
-	}
-	return nil
 }
